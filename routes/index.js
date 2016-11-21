dc33765b53b1f9332fc82d86d41471a939f1b520
justin73 <jiameng_73@hotmail.com>
2016-11-20 22:29:41 -0500

    add head image

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
 exports.index = function(req, res){
-  res.render('index', { name: 'John' });
+	response = res
+	var request = require('request');
+	var str2json = require('string-to-json');
+	request({
+	    headers: {
+				    'Accept': 'application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/',
+				    'X-Busbud-Token': 'PARTNER_JSWsVZQcS_KzxNRzGtIt1A'
+				},
+	    uri: 'https://napi.busbud.com/x-departures/dr5reg/f25dvk/2017-07-03?adult=1&child=0&senior=0&lang=en&currency=CAD',
+	    method: 'GET'
+	  }, function (err, res, body) {
+	  	console.log(typeof body)
+	  	console.log(JSON.parse(body))
+	    response.render('index');
+  	});
 };
--- views/index.jsx
+++ views/index.jsx
@@ -1,8 +1,55 @@
 var React = require('react');
+var DefaultLayout = require('./layouts/default');
+
 
 class HelloMessage extends React.Component {
+  handleOptionsButtonClick(e) {
+  	fetch('https://napi.busbud.com/x-departures/dr5reg/f25dvk/2017-07-03/poll?adult=1&child=0&senior=0&lang=en&currency=CAD').then((response) => {
+          return response.json();
+      }).catch((err) => {
+          throw new Error(err);
+      });
+  }
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <DefaultLayout>
+        <div className="banner_background">
+			<img src="/logo.png"></img>
+		</div>
+		<div>
+			<form>
+				<div>
+					<input className="depature" value="NEW YORK" readOnly></input>
+					<input className="destination" value="MONTREAL" readOnly></input>
+				</div>
+				<div>
+					<label>
+						<input type="radio" name="age" value="adult" checked>
+
+						</input>
+
+						Adult
+					</label>
+					<label>
+						<input type="radio" name="age" value="senior">
+
+						</input>
+
+						Senior
+					</label>
+					<label>
+						<input type="radio" name="age" value="child">
+
+						</input>
+
+						Child
+					</label>
+				</div>
+				<button className="search"  onClick={this.props.handleOptionsButtonClick}> Search</button>
+			</form>
+		</div>
+      </DefaultLayout>
+    );
   }
 }
 
