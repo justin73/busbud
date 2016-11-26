3512a840e89d8f24402cadd581ceee26fdde12f9
justin73 <jiameng_73@hotmail.com>
2016-11-26 16:07:46 -0500

    change to react

--- public/js/test.js
+++ public/js/test.js
@@ -1,5 +1,29 @@
-$(document).ready(function () {
-    $(".search").on("click",function(){
-        console.log("search click")
-    })
-}); //doc.ready
+/**
+ * @jsx React.DOM
+ */
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+
+  render: function () {
+    return(
+        <section>
+            <div className="banner_background">
+                <img src="/logo.png"></img>
+            </div>
+            <div>
+                <div>
+                    <input className="depature" value="NEW YORK" readOnly></input>
+                    <input className="destination" value="MONTREAL" readOnly></input>
+                </div>
+                <button className="search"> Search</button>
+            </div>
+        </section>
+    )
+  }
+});
+React.render(
+    <App />,
+    document.getElementById('index')
+);
